d665dacaa0cc2aa881f71c708a5dcf4aaf1bb518
Jordon Bedwell <jordon@envygeeks.io>
2015-11-03 19:48:29 -0600

    Modernize Kramdown for Markdown converter.

--- lib/jekyll/converters/markdown/kramdown_parser.rb
+++ lib/jekyll/converters/markdown/kramdown_parser.rb
@@ -1,33 +1,84 @@
+# Frozen-string-literal: true
+# Encoding: utf-8
+
 module Jekyll
   module Converters
     class Markdown
       class KramdownParser
+        CODERAY_DEFAULTS = {
+          "bold_every" => 10,
+          "css" => "style",
+          "line_numbers" => "inline",
+          "line_number_start" => 1,
+          "wrap" => "div",
+          "tab_width" => 4
+        }
+
         def initialize(config)
-          require 'kramdown'
-          @config = config
-          # If kramdown supported highlighter enabled, use that
-          highlighter = @config['highlighter']
-          if highlighter == 'rouge' || highlighter == 'coderay'
-            @config['kramdown']['syntax_highlighter'] ||= highlighter
-          end
-        rescue LoadError
-          STDERR.puts 'You are missing a library required for Markdown. Please run:'
-          STDERR.puts '  $ [sudo] gem install kramdown'
-          raise Errors::FatalException.new("Missing dependency: kramdown")
+          Jekyll::External.require_with_graceful_fail "kramdown"
+          @main_fallback_highlighter = config["highlighter"] || "rogue"
+          @config = config["kramdown"] || {}
+          setup
+        end
+
+        # Setup and normalize the configuration:
+        #   * Create Kramdown if it doesn't exist.
+        #   * Set syntax_highlighter, detecting enable_coderay and merging highlighter if none.
+        #   * Merge kramdown[coderay] into syntax_highlighter_opts stripping coderay_.
+        #   * Make sure `syntax_highlighter_opts` exists.
+
+        def setup
+          @config["syntax_highlighter"] ||= highlighter
+          @config["syntax_highlighter_opts"] ||= {}
+          @config["coderay"] ||= {} # XXX: Legacy.
+          modernize_coderay_config
         end
 
         def convert(content)
-          # Check for use of coderay
-          if @config['kramdown']['enable_coderay']
-            %w[wrap line_numbers line_numbers_start tab_width bold_every css default_lang].each do |opt|
-              key = "coderay_#{opt}"
-              @config['kramdown'][key] = @config['kramdown']['coderay'][key] unless @config['kramdown'].key?(key)
-            end
-          end
-
-          Kramdown::Document.new(content, Utils.symbolize_hash_keys(@config['kramdown'])).to_html
+          Kramdown::Document.new(content, @config).to_html
         end
 
+        # config[kramdown][syntax_higlighter] > config[kramdown][enable_coderay] > config[highlighter]
+        # Where `enable_coderay` is now deprecated because Kramdown
+        # supports Rouge now too.
+
+        private
+        def highlighter
+          @highlighter ||= begin
+            if out = @config["syntax_highlighter"] then out
+            elsif @config.has_key?("enable_coderay") && @config["enable_coderay"]
+              Jekyll.logger.warn "You are using enable_coderay, use syntax_highlighter: coderay." \
+                "In the future enable_coderay will be removed entirely."
+              "coderay"
+            else
+              @main_fallback_highlighter
+            end
+          end
+        end
+
+        private
+        def strip_coderay_prefix(hash)
+          hash.inject({}) do |hash_, (key, val)|
+            cleaned_key = key.gsub(/\Acoderay_/, "")
+            if hash.has_key?(key)
+              Jekyll.logger.warn "You are using an old CodeRay key: '#{key}'." \
+                "It is being normalized to #{cleaned_key}."
+            end
+
+            hash_.update({
+              cleaned_key => val
+            })
+          end
+        end
+
+        private
+        def modernize_coderay_config
+          if highlighter == "coderay"
+            @config["syntax_highlighter_opts"] = strip_coderay_prefix(
+              @config["syntax_highlighter_opts"].merge(CODERAY_DEFAULTS).merge(@config["coderay"])
+            )
+          end
+        end
       end
     end
   end
